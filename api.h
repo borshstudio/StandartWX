68a168ea2ae4f1fbb8286f165755dce2ce388867
Дмитрий <borshstudio@mail.ru>
2026-05-06 15:07:22 +0300

    Update api.h

--- api.h
+++ api.h
@@ -1,1 +1,16 @@
+#ifndef WEATHER_H
+#define WEATHER_H
 
+#include <string>
+#include <vector>
+
+void showWeatherByCity(const std::string& apiKey);
+void showWeatherForCity(const std::string& city, const std::string& apiKey);
+
+std::vector<std::string> loadFavorites();
+void saveFavorites(const std::vector<std::string>& favorites);
+
+void favoritesMenu(const std::string& apiKey);
+void aboutApp();
+
+#endif
