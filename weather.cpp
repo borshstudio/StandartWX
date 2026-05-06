fcbd0e510a9b09f9d4862870edf0016f40c28e72
3Jlou <uranov2017@gmail.com>
2026-05-07 02:08:55 +0300

    добавлены функции работы с избранными городами

--- weather.cpp
+++ weather.cpp
@@ -59,19 +59,88 @@ void showWeatherByCity(const std::string& apiKey) {
 
 
 std::vector<std::string> loadFavorites() {
-    // ToDo
+    std::vector<std::string> favorites;
+    std::ifstream file("favorites.txt");
+    std::string city;
+
+    while (std::getline(file, city)) {
+        if (!city.empty()) favorites.push_back(city);
+    }
+    return favorites;
 }
 
 
 
 void saveFavorites(const std::vector<std::string>& favorites) {
-    // ToDo
+    std::ofstream file("favorites.txt");
+    for (const auto& city : favorites) {
+        file << city << "\n";
+    }
 }
 
 
 
 void favoritesMenu(const std::string& apiKey) {
-    // ToDo
+    while (true) {
+        std::cout << "\n=== ИЗБРАННЫЕ ГОРОДА ===\n";
+        std::cout << "1) Показать список\n";
+        std::cout << "2) Добавить город\n";
+        std::cout << "3) Удалить город\n";
+        std::cout << "4) Показать погоду\n";
+        std::cout << "0) Назад\n";
+        std::cout << "Ваш выбор: ";
+
+        std::string choice;
+        std::getline(std::cin, choice);
+        auto favorites = loadFavorites();
+        [06.05.2026 15:24] ? ? ? ? s ? ? ? ? ? ? ? ? : if (choice == "1") {
+            if (favorites.empty()) {
+                std::cout << "Список пуст.\n";
+            }
+            else {
+                for (int i = 0; i < favorites.size(); ++i)
+                    std::cout << i + 1 << ") " << favorites[i] << "\n";
+            }
+        }
+        else if (choice == "2") {
+            std::string city;
+            std::cout << "Введите город: ";
+            std::getline(std::cin, city);
+            if (!city.empty()) {
+                favorites.push_back(city);
+                saveFavorites(favorites);
+                std::cout << "[OK] Город добавлен.\n";
+            }
+        }
+        else if (choice == "3") {
+            if (favorites.empty()) {
+                std::cout << "Список пуст.\n";
+                continue;
+            }
+            std::cout << "Введите номер: ";
+            std::string n;
+            std::getline(std::cin, n);
+            int idx = std::stoi(n) - 1;
+            if (idx >= 0 && idx < favorites.size()) {
+                favorites.erase(favorites.begin() + idx);
+                saveFavorites(favorites);
+                std::cout << "[OK] Город удален.\n";
+            }
+        }
+        else if (choice == "4") {
+            if (favorites.empty()) continue;
+            std::cout << "Введите номер: ";
+            std::string n;
+            std::getline(std::cin, n);
+            int idx = std::stoi(n) - 1;
+            if (idx >= 0 && idx < favorites.size()) {
+                showWeatherForCity(favorites[idx], apiKey);
+            }
+        }
+        else if (choice == "0") {
+            return;
+        }
+    }
 }
 
 
