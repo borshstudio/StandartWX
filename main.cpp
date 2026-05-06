31b9deab133f51981e79147c64bb3773bdf5c4cc
7grach <grachev.alexandr001@gmail.com>
2026-05-06 17:13:35 +0300

    добавил функцию getValidApiKey(), которая делает проверку api ключа с вводоом пользователя

--- main.cpp
+++ main.cpp
@@ -20,42 +20,45 @@ void saveApiKey(const std::string& apiKey) {
     file << apiKey;
 }
 
+std::string getValidApiKey() {
+    std::string apiKey = loadApiKey();
+
+     while (true) {
+        std::cout << "\nВведите API-ключ OpenWeatherMap (0 — выход): ";
+        std::getline(std::cin, apiKey);
+
+        if (apiKey == "0") {
+            return "";
+        }
+
+        if (apiKey.empty()) {
+            std::cout << "[ERROR] API-ключ не может быть пустым.\n";
+            continue;
+        }
+
+        std::cout << "[INFO] Проверяем API-ключ...\n";
+
+        if (checkApiKey(apiKey)) {
+            saveApiKey(apiKey);
+            std::cout << "[OK] API-ключ сохранен.\n";
+            return apiKey;
+        }
+
+        std::cout << "[ERROR] API-ключ неправильный.\n";
+    }
+}
+
+
+
 int main() {
-    int choice;
 
-    std::cout << "=== СтандартWX - Прогноз погоды ===" << std::endl;
+    std::cout << "Добро пожаловать в приложение прогноза погоды!\n";
 
-    do {
-
-        std::cout << "\nВыберите опцию:" << std::endl;
-        std::cout << "1. Текущая погода" << std::endl;
-        std::cout << "2. Прогноз погоды на 3 дня" << std::endl;
-        std::cout << "3. Выход" << std::endl;
-        std::cout << "Ваш выбор: ";
-
-        if (!(std::cin >> choice)) {
-            std::cout << "\nОшибка ввода. Пожалуйста, введите число." << std::endl;
-            std::cin.clear();
-            std::cin.ignore(10000, '\n');
-            choice = 0;
-        }
-
-        switch(choice) {
-            case 1:
-                std::cout << "\n" << getCurrentWeather() << std::endl;
-                break;
-            case 2:
-                std::cout << "\n" << getForecastWeather() << std::endl;
-                break;
-            case 3:
-                std::cout << "\nДо свидания!" << std::endl;
-                break;
-            default:
-                std::cout << "\nНеверный выбор. Попробуйте снова." << std::endl;
-                break;
-        }
-
-    } while(choice != 3);
+    std::string apiKey = getValidApiKey();
+    if (apiKey.empty()) {
+        std::cout << "Выход из приложения.\n";
+        return 0;
+    }
 
     return 0;
 }
